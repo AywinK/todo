9b824566a601c379cb9aaeb31d581619c3edd4fc
Aywin <aywink@hotmail.co.uk>
2023-11-04 12:47:43 +0000

    initial method

--- public/app.js
+++ public/app.js
@@ -1,57 +1,34 @@
-const todos = document.getElementById("todos");
-console.log(todos);
+const todosEl = document.getElementById("todos");
 
-const todosForm = document.getElementById("todos-form");
-todosForm.addEventListener("submit", appendTodo);
 
-function appendTodo(e) {
-  e.preventDefault();
-  console.log(e.target[0].value);
-  if (e.target[0].value !== "") {
-    addTodo(e.target[0].value);
-  }
-  e.target[0].value = "";
-}
-
-function addTodo(todoStr) {
-  const todo = document.createElement("li");
-  todo.textContent = todoStr;
-  todo.appendChild(addEditBtn(todoStr));
-  todos.appendChild(todo);
-}
-
-function addEditBtn(todoStr) {
+function createEditBtn() {
   const editBtn = document.createElement("button");
-  editBtn.value = todoStr;
-  editBtn.setAttribute("type", "button");
-  editBtn.textContent = "Edit";
+  editBtn.type = "button";
+  editBtn.value = "edit";
+  editBtn.innerText = "Edit";
   return editBtn;
 }
 
-todos.addEventListener("click", (e) => {
-  console.log(e.target.type);
-  if (e.target.type === "button") {
-    console.log(e.target.value);
-    const updatedTodoVal = prompt(`Enter updated todo for "${e.target.value}"`);
-    const editBtns = document.querySelectorAll('button[type="button"]');
-    for (let i = 0; i < editBtns.length; i++) {
-      if (editBtns[i].value === e.target.value) {
-        editBtns[i].value = updatedTodoVal;
-      }
-    }
-    const listElsArr = document.querySelectorAll("li");
-    console.log(listElsArr[0]);
-    for (let i = 0; i < listElsArr.length; i++) {
-      console.log(listElsArr[i]);
-      if (listElsArr[i].value === e.target.value) {
-        listElsArr[i].value = updatedTodoVal;
-        listElsArr[i].innerText = updatedTodoVal;
-      }
-    }
-    console.log(editBtns);
-  }
-});
+function createDeleteBtn() {
+  const delBtn = document.createElement("button");
+  delBtn.type = "button";
+  delBtn.value = "delete";
+  delBtn.innerText = "Delete";
+  return delBtn;
+}
 
-for (let i = 0; i <= 10; i++) {
-  addTodo(i);
+function createParagraphTag(todoStr) {
+  const p = document.createElement("p");
+  p.value = "todo";
+  p.innerText = todoStr;
+  return todoStr;
+}
+
+function createTodo(todoStr, todos = todosEl) {
+  const listEl = document.createElement("li");
+  listEl.value = todoStr;
+  listEl.appendChild(createParagraphTag(todoStr));
+  listEl.appendChild(createEditBtn());
+  listEl.appendChild(createDeleteBtn());
+  todos.appendChild(listEl);
 }
